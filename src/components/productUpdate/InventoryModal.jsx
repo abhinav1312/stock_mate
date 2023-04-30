45def51092ad9d6a6b4678778ffdc3ca6d7b52d5
Abhinav <abhinavsrivastav699@gmail.com>
2023-04-30 16:23:52 +0530

    modal ui refactoring

--- src/components/productUpdate/InventoryModal.jsx
+++ src/components/productUpdate/InventoryModal.jsx
@@ -3,64 +3,80 @@ import React from 'react'
 const InventoryModal = ({inventoryInfoSubmit, name, category, brand, handleContentChange, quantity, expiryDate}) => {
   return (
     <>
-     <div className="absolute bottom-72 inset-0 opacity-80 bg-gray-300 flex justify-center items-center  h-full w-full"></div>
-      <div className="relative p-16 bg-white w-3/4 z-10 rounded-xl shadow-md">
-        <h3 className="pb-4">
+     <div className="absolute z-10 inset-0 opacity-80 bg-gray-300  overflow-hidden"></div>
+      <div className="fixed top-16 left-[25rem] z-10 p-8 rounded-2xl shadow-lg bg-white w-1/2">
+        <h2 className="pb-4 text-2xl font-medium">
           Please fill and update necessary details
-        </h3>
+        </h2>
         <form
           onSubmit={inventoryInfoSubmit}
-          className="flex flex-col justify-between gap-8"
+          className="flex flex-col justify-between gap-4"
         >
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Enter product name"
-            value={name}
-            onChange={handleContentChange}
-            className="p-4 text-2xl focus:border-blue-400 rounded-2xl focus:outline-none border"
-          />
-          <input
-            type="text"
-            name="category"
-            id="category"
-            placeholder="Enter product category for eg: Chips, biscuit, sauce, spread etc.."
-            value={category}
-            onChange={handleContentChange}
-            className="p-4 text-2xl focus:border-blue-400 rounded-2xl focus:outline-none border"
-          />
-          <input
-            type="text"
-            name="brand"
-            id="brand"
-            placeholder="Enter product brand"
-            value={brand}
-            onChange={handleContentChange}
-            className="p-4 text-2xl focus:border-blue-400 rounded-2xl focus:outline-none border"
-          />
-          <input
-            type="number"
-            name="quantity"
-            id="quantity"
-            placeholder="Enter quantity"
-            value={quantity}
-            onChange={handleContentChange}
-            className="p-4 text-2xl focus:border-blue-400 rounded-2xl focus:outline-none border"
-          />
-          <input
-            type="date"
-            name="expiryDate"
-            id="expiryDate"
-            placeholder="Enter expiry date"
-            value={expiryDate}
-            onChange={handleContentChange}
-            className="p-4 text-2xl focus:border-blue-400 rounded-2xl focus:outline-none border"
-          />
+          <div>
+            <h3 className='modalFormHeading'>Product name</h3>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter product name"
+              value={name}
+              onChange={handleContentChange}
+              className="modalFormInput"
+            />
+          </div>
+          <div>
+            <h3 className='modalFormHeading'> Category </h3>
+            <input
+              type="text"
+              name="category"
+              id="category"
+              placeholder="Enter product category for eg: Chips, biscuit, sauce, spread etc.."
+              value={category}
+              onChange={handleContentChange}
+              className="modalFormInput"
+            />
+          </div>
+
+          <div>
+            <h3 className='modalFormHeading'> Brand </h3>
+            <input
+              type="text"
+              name="brand"
+              id="brand"
+              placeholder="Enter product brand"
+              value={brand}
+              onChange={handleContentChange}
+              className="modalFormInput"
+            />
+          </div>
+          <div>
+            <h3 className='modalFormHeading'> Quantity </h3>
+            <input
+              type="number"
+              name="quantity"
+              id="quantity"
+              placeholder="Enter quantity"
+              value={quantity}
+              onChange={handleContentChange}
+              className="modalFormInput"
+            />
+          </div>
+          <div>
+            <h3 className='modalFormHeading'> Expiry date </h3>
+            <input
+              type="date"
+              name="expiryDate"
+              id="expiryDate"
+              placeholder="Enter expiry date"
+              value={expiryDate}
+              onChange={handleContentChange}
+              className="modalFormInput"
+            />
+          </div>
 
           <button
             type="submit"
-            className="px-16 py-8 border hover:bg-gray-300 text-2xl font-semibold bg-blue hover:bg-hoverBlue"
+            className="px-8 py-4 rounded-md border bg-green-600 hover:bg-green-700 text-white text-xl font-semibold"
           >
             Submit
           </button>
